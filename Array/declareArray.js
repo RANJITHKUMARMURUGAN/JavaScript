ae7c1afa7012a147e0cff05c2d420d9b9c0e3ae0
ranjithkumar <ranjithkumar.m@perfios.com>
2023-09-29 21:05:28 +0530

    created Array concept in javascript

--- Array/declareArray.js
+++ Array/declareArray.js
@@ -1,6 +1,50 @@
-var temp = "Ranjithkumar";
-console.log("the variable" + " " + temp);
-const temp = function () {
-    var sum = 10 + 20;
-}
+//decalration basic Array iniliazation
+// /*
+const friends = ['apple', 'banana', 'watermelon'];
+console.log(friends);
+const temp = new Array(1991, 1992, 1993);
 console.log(temp);
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends.length - 1);
+friends[1] = 'ranjith';
+console.log(friends)
+const calcAge = function (birthYear) {
+    return 2037 - birthYear
+}
+const years = [1990, 1991, 1992, 1993, 1994];
+console.log(calcAge(years[0]));
+// */
+
+//Added the Value lish Push() method
+// /*
+const friends = ['ranjith', 'kumar', 'hatim', 'maveeran'];
+friends.push('Naveen');
+console.log(friends);
+const pushed = friends.push('Naveen')
+console.log(pushed);
+console.log(friends.length);
+friends.unshift('Alloment')
+console.log(friends);
+// */
+//pop the Value lish Pop() method
+// /*
+friends.pop();
+console.log(friends);
+friends.shift();
+console.log(friends);
+const removeValue = friends.pop()
+console.log(removeValue);
+console.log(friends);
+console.log(friends.indexOf('maveeran'));
+console.log(friends.indexOf('dsfsdfsdfsdf'));
+console.log(friends.length);
+console.log(friends.includes('maveeran'));  //checking true or false
+console.log(friends.includes('dgdfgdfgdfgdf'));  //checking true or false in  conditional
+friends.push(25);
+console.log(friends.includes('25'))   // false 25 is not a string 
+console.log(friends.includes(25))   // true 25 is not a number 
+if (friends.includes('kumar')) {
+    console.log("you have friend")
+}
+// */
